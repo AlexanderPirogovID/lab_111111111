65558c9bc68dc609ed1ae5784ef43e3f06686862
AlexanderPirogovID <andrey.v.pirogov@gmail.com>
2025-09-06 14:21:07 +0300

    Add files via upload

---
+++ task_5.py
@@ -0,0 +1,6 @@
+numbers = [12, -5, 23, 0, 4, 17, -8, 0, 11]
+print("Количество элементов: ", len(numbers))
+print("Помледний элемент: ", numbers[-1])
+print("В обратном порядке: ", numbers[::-1])
+if 5 in numbers and 17 in numbers: print("Да")
+else: print("Нет")
